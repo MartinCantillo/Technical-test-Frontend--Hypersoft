5733934478a0c5160883c692d2f307a636afd6d9
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-10-16 07:52:21 -0500

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponentComponent } from './components/product-component/product-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponentComponent
   ],
   imports: [
     BrowserModule
